fbee1e25809391294fe5c84c3deee6788c1842b7
Dean Djermanović <dean74745@gmail.com>
2017-05-02 17:25:41 +0200

    Add content provider

--- app/src/main/java/com/example/dean/weatherreport/data/WeatherDatabase.java
+++ app/src/main/java/com/example/dean/weatherreport/data/WeatherDatabase.java
@@ -1,11 +1,18 @@
 package com.example.dean.weatherreport.data;
 
 import com.raizlabs.android.dbflow.annotation.Database;
+import com.raizlabs.android.dbflow.annotation.provider.ContentProvider;
 
-@Database(name = WeatherDatabase.DB_NAME, version = WeatherDatabase.DB_VERSION)
+@ContentProvider(authority = WeatherDatabase.AUTHORITY,
+        database = WeatherDatabase.class,
+        baseContentUri = WeatherDatabase.BASE_CONTENT_URI)
+@Database(name = WeatherDatabase.DB_NAME,
+        version = WeatherDatabase.DB_VERSION)
 public class WeatherDatabase {
 
     public static final String DB_NAME = "weather";
-
     public static final int DB_VERSION = 1;
+
+    public static final String BASE_CONTENT_URI = "content://";
+    public static final String AUTHORITY = "com.example.dean.weatherreport.data";
 }
--- app/src/main/java/com/example/dean/weatherreport/data/WeatherTable.java
+++ app/src/main/java/com/example/dean/weatherreport/data/WeatherTable.java
@@ -1,18 +1,32 @@
 package com.example.dean.weatherreport.data;
 
+import android.net.Uri;
+
 import com.raizlabs.android.dbflow.annotation.Column;
 import com.raizlabs.android.dbflow.annotation.ConflictAction;
 import com.raizlabs.android.dbflow.annotation.NotNull;
 import com.raizlabs.android.dbflow.annotation.PrimaryKey;
 import com.raizlabs.android.dbflow.annotation.Table;
 import com.raizlabs.android.dbflow.annotation.Unique;
-import com.raizlabs.android.dbflow.structure.BaseModel;
+import com.raizlabs.android.dbflow.annotation.provider.ContentUri;
+import com.raizlabs.android.dbflow.annotation.provider.TableEndpoint;
+import com.raizlabs.android.dbflow.structure.provider.BaseProviderModel;
+import com.raizlabs.android.dbflow.structure.provider.ContentUtils;
 
-@Table(database = WeatherDatabase.class)
-public class WeatherTable extends BaseModel {
+@TableEndpoint(name = WeatherTable.NAME,
+        contentProvider = WeatherDatabase.class)
+@Table(database = WeatherDatabase.class,
+        name = WeatherTable.NAME)
+public class WeatherTable extends BaseProviderModel {
+
+    public static final String NAME = "WeatherTable";
+
+    @ContentUri(path = NAME, type = ContentUri.ContentType.VND_MULTIPLE + NAME)
+    public static final Uri CONTENT_URI =
+            ContentUtils.buildUriWithAuthority(WeatherDatabase.AUTHORITY);
 
     @Column
-    @PrimaryKey
+    @PrimaryKey(autoincrement = true)
     int id;
 
     @Column
@@ -47,4 +61,24 @@ public class WeatherTable extends BaseModel {
     @Column
     @NotNull
     Double degrees;
+
+    @Override
+    public Uri getDeleteUri() {
+        return WeatherTable.CONTENT_URI;
+    }
+
+    @Override
+    public Uri getInsertUri() {
+        return WeatherTable.CONTENT_URI;
+    }
+
+    @Override
+    public Uri getUpdateUri() {
+        return WeatherTable.CONTENT_URI;
+    }
+
+    @Override
+    public Uri getQueryUri() {
+        return WeatherTable.CONTENT_URI;
+    }
 }
